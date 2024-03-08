7d6b0569d5045a5fb87d121b7390bb75574f379a
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-03-08 17:40:52 +0300

    completed

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,43 @@
 class Pet:
-    pass
+    all=[]
+    PET_TYPES=["dog", "cat", "rodent", "bird", "reptile",'exotic']
+    def __init__(self,name,pet_type):
+        self.name = name
+        self.pet_type = pet_type
+        self._owner=None
+        Pet.all.append(self)
+     
+    @property
+    def owner(self):
+             return self._owner
+    @owner.setter
+    def owner(self,owner):
+        self._owner = owner
+    @property
+    def pet_type(self):
+        return self.pet_type
+    @pet_type.setter
+    def pet_type(self,pet_type):
+        if pet_type not in Pet.PET_TYPES:
+            raise ValueError("pet_type not valid")
+        self._pet_type = pet_type
+        
 
 class Owner:
-    pass
+    def __init__(self,name):
+        self.name = name
+        self.add_pet=""
+        
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner ==self]
+    
+    def add_pet(self,pet):
+        if isinstance(pet,Pet):
+            Pet.all.append(pet)
+            
+    def get_sorted_pets(self):
+        return sorted(Pet.all)
+    
+    
+    Simon=Pet("Simon","bird")
+    
