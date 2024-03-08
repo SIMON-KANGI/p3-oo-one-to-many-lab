03710c636cfb43ee76b46e5b6732a8af771efda7
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-03-08 17:49:36 +0300

    completed

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,10 +1,10 @@
 class Pet:
     all=[]
     PET_TYPES=["dog", "cat", "rodent", "bird", "reptile",'exotic']
-    def __init__(self,name,pet_type):
+    def __init__(self,name,pet_type,owner=None):
         self.name = name
         self.pet_type = pet_type
-        self._owner=None
+        self._owner=owner
         Pet.all.append(self)
      
     @property
@@ -26,14 +26,16 @@ class Pet:
 class Owner:
     def __init__(self,name):
         self.name = name
-        self.add_pet=""
+        #self.add_pet =None
         
     def pets(self):
         return [pet for pet in Pet.all if pet.owner ==self]
     
     def add_pet(self,pet):
-        if isinstance(pet,Pet):
-            Pet.all.append(pet)
+        if not isinstance(pet,Pet):
+            raise TypeError("pet must be of type Pet")
+        pet.owner=self
+           
             
     def get_sorted_pets(self):
         return sorted(Pet.all)
